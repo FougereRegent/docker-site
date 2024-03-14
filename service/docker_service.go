5e1e0850c45bc822a290d0e6685c0d04bacfdf40
FougereRegent <damien.venant@outlook.com>
2024-03-14 17:15:05 +0000

    Ajout du dashboard
    ajout d'une image de logo
    
    Modification de la bar des menues
    
    Modification dashboard
    
    Ajout d'un service pour la gestions de docker

---
+++ service/docker_service.go
@@ -0,0 +1,44 @@
+package service
+
+const (
+	CONTAINER_TYPE ElementType = "container"
+	IMAGE_TYPE     ElementType = "image"
+	VOLUME_TYPE    ElementType = "volume"
+	NETWORK_TYPE   ElementType = "network"
+)
+
+type ElementType string
+
+type Resume struct {
+	Type      ElementType
+	NbElement int
+}
+
+type ContainerResume struct {
+	Resume
+	NbActive int
+}
+
+type ImageResume struct {
+	Resume
+}
+
+type VolumeResume struct {
+	Resume
+}
+
+type NetworkResume struct {
+	Resume
+	NbDriver int
+	NbHost   int
+	NbBridge int
+}
+
+func GetContainerResume() {
+}
+
+func GetImageResume() {
+}
+
+func GetVolumeResume() {
+}
