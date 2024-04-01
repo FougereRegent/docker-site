d1fae4da8c154284af6501624694ce35a5fa1d5a
FougereRegent <damien.venant@outlook.com>
2024-04-01 18:38:44 +0200

    Refactorisation des images

--- dto/docker_dto.go
+++ dto/docker_dto.go
@@ -1,5 +1,9 @@
 package dto
 
+import (
+	"time"
+)
+
 const (
 	CONTAINER_TYPE ElementType = "Container"
 	IMAGE_TYPE     ElementType = "Image"
@@ -37,4 +41,53 @@ type NetworkResume struct {
 	NbOverlay int
 	NbIpVlan  int
 	NbMacVlan int
+}
+
+type DockerNetwork struct {
+	Name       string    `json:"Name"`
+	Id         string    `json:"Id"`
+	Created    time.Time `json:"Created"`
+	Scope      string    `json:"Scope"`
+	Driver     string    `json:"Driver"`
+	EnableIPv6 bool      `json:"EnableIPv6"`
+	Internal   bool      `json:"Internal"`
+	Attachable bool      `json:"Attachable"`
+	Ingress    bool      `json:"Ingress"`
+}
+
+type DockerVolume struct {
+	Volumes []struct {
+		Name       string                 `json:"Name"`
+		Driver     string                 `json:"Driver"`
+		MountPoint string                 `json:"MountPoint"`
+		CreatedAt  time.Time              `json:"CreatedAt"`
+		Status     map[string]interface{} `json:"Status"`
+		Labels     map[string]interface{} `json:"Labels"`
+		Scope      string                 `json:"Scope"`
+	} `json:"Volumes"`
+}
+
+func (c *DockerNetwork) CountElement(network *NetworkResume) {
+	switch c.Driver {
+	case "host":
+		network.NbHost += 1
+		break
+	case "bridge":
+		network.NbBridge += 1
+		break
+	case "none":
+		network.NbNone += 1
+		break
+	case "overlay":
+		network.NbOverlay += 1
+		break
+	case "macvlan":
+		network.NbMacVlan += 1
+		break
+	case "ipvlan":
+		network.NbIpVlan += 1
+		break
+	default:
+		return
+	}
 }
