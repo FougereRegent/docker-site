6507ffa30ce2495892ac8dba7aaf8200a8b5ba30
FougereRegent <damien.venant@outlook.com>
2024-04-01 18:43:11 +0200

    Refactorisation des réseaux

--- dto/docker_dto.go
+++ dto/docker_dto.go
@@ -43,18 +43,6 @@ type NetworkResume struct {
 	NbMacVlan int
 }
 
-type DockerNetwork struct {
-	Name       string    `json:"Name"`
-	Id         string    `json:"Id"`
-	Created    time.Time `json:"Created"`
-	Scope      string    `json:"Scope"`
-	Driver     string    `json:"Driver"`
-	EnableIPv6 bool      `json:"EnableIPv6"`
-	Internal   bool      `json:"Internal"`
-	Attachable bool      `json:"Attachable"`
-	Ingress    bool      `json:"Ingress"`
-}
-
 type DockerVolume struct {
 	Volumes []struct {
 		Name       string                 `json:"Name"`
@@ -65,29 +53,4 @@ type DockerVolume struct {
 		Labels     map[string]interface{} `json:"Labels"`
 		Scope      string                 `json:"Scope"`
 	} `json:"Volumes"`
-}
-
-func (c *DockerNetwork) CountElement(network *NetworkResume) {
-	switch c.Driver {
-	case "host":
-		network.NbHost += 1
-		break
-	case "bridge":
-		network.NbBridge += 1
-		break
-	case "none":
-		network.NbNone += 1
-		break
-	case "overlay":
-		network.NbOverlay += 1
-		break
-	case "macvlan":
-		network.NbMacVlan += 1
-		break
-	case "ipvlan":
-		network.NbIpVlan += 1
-		break
-	default:
-		return
-	}
 }
