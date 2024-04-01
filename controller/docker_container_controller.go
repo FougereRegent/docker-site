63060c570c9252ec40d3f6737a837a940b8c80ab
FougereRegent <damien.venant@outlook.com>
2024-04-01 18:57:19 +0200

    Ajout des primitives de gestions de container

--- controller/docker_container_controller.go
+++ controller/docker_container_controller.go
@@ -1,1 +1,35 @@
 package controller
+
+import "github.com/gin-gonic/gin"
+
+//This files groups every containers controller like this :
+// - Delete
+// - Creation
+// - Start
+// - Stop
+// - Restart
+
+// TODO : Implements deleting container in docker engine
+func DeleteContainer(c *gin.Context) {
+
+}
+
+// TODO : Implements creating container in docker engine
+func CreateContainer(c *gin.Context) {
+
+}
+
+// TODO : Implements starting container in docker engine
+func StartContainer(c *gin.Context) {
+
+}
+
+// TODO : Implements stopping container in docker engine
+func StopContainer(c *gin.Context) {
+
+}
+
+// TODO : Implements restarting container in docker engine
+func RestartContainer(c *gin.Context) {
+
+}
